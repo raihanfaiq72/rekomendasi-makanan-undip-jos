5bdeaa789ebbb2e1dbc205c0120a16b4182acd7b
IQ72 <105931001+raihanfaiq72@users.noreply.github.com>
2024-11-12 04:32:35 +0700

    perbaikan tipis

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PenjualController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +18,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('dashboard-main',[DashboardController::class,'index']);
+    Route::get('penjual',[PenjualController::class , 'index']);
 });
 
 require __DIR__.'/auth.php';
